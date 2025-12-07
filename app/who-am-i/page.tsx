87f79fa17ac72e0adbb03d11168dfc24cc091bf4
Jake Spencer Walklate <47465827+svnty@users.noreply.github.com>
2025-12-08 06:11:36 +1100

    Update page.tsx

--- app/who-am-i/page.tsx
+++ app/who-am-i/page.tsx
@@ -351,6 +351,7 @@ export default function WhoAmIPage() {
             <li><X className="text-red-400 inline me-1.5 shrink-0" />Difficult English spelling</li>
             <li><X className="text-red-400 inline me-1.5 shrink-0" />Catching buses</li>
             <li><X className="text-red-400 inline me-1.5 shrink-0" />Loans and compound interest</li>
+            <li><X className="text-red-400 inline me-1.5 shrink-0" />Human suffering</li>
           </ul>
         </div>
       </section>
