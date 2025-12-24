cb084b3c0a76eecf7a924d6d334663bae60737cc
Jake Walklate <jakewalklate@protonmail.com>
2025-12-25 03:39:39 +1100

    nav

--- app/nav.tsx
+++ app/nav.tsx
@@ -10,7 +10,7 @@ export default function DynamicNav() {
     <nav className="flex gap-6 text-sm font-medium text-zinc-400">
       <Link href="/who-am-i" className={`${segment === "who-am-i" ? 'text-white' : ''} hover:text-white transition-colors mx-1 active:scale-[0.98]`}>Bio</Link>
       <Link href="/work" className={`${segment === 'work' ? 'text-white' : ''} hover:text-white transition-colors mx-1 active:scale-[0.98]`}>Work</Link>
-      <Link href="/blog" className={`${segment === 'blog' ? 'text-white' : ''} hover:text-white transition-colors mx-1 active:scale-[0.98]`}>Writing</Link>
+      <Link href="/blog" className={`${segment === 'blog' ? 'text-white' : ''} hover:text-white transition-colors mx-1 active:scale-[0.98]`}>Blog</Link>
     </nav>
   );
 }
