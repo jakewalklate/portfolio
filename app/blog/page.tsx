c2d0d33fedd04bd6e40844859318bbb125b28e96
Jake Walklate [@svnty] <jake@svnty.is-a.dev>
2026-01-17 10:58:10 +0900

    compress

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -61,7 +61,7 @@ export default async function Blog() {
         >
           <div className="relative shrink-0 h-28 w-20 my-auto -rotate-1 group-hover:rotate-0 transition-transform duration-500 overflow-hidden rounded-lg shadow-lg border border-white/10">
             <img
-              src="/5x8.png"
+              src="/5x8-compressed.jpg"
               alt="The Pharmacology of Silence"
               className="h-full w-full object-cover"
             />
