1c9fd9e612a97126be2c8a60ac1c7ddbe3c3960c
svnty <jakewalklate@gmail.com>
2026-05-09 00:30:39 +1000

    .

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,7 +23,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/book`,
+      url: `${baseUrl}/book/the-pharmacology-of-silence`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
